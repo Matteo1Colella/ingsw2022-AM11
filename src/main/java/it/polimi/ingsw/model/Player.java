cc493cb8d038d89689fcda85c032a7804b5f2517
Aleksandro Aliaj <100211777+AleksandroAliaj@users.noreply.github.com>
2022-03-14 22:53:43 +0100

    add player class (#7)

---
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.model;
+
+public class Player {
+    int PlayerNum;
+
+    public Player(int playerNum) {
+        PlayerNum = playerNum;
+    }
+
+    public int getPlayerNum() {
+        return PlayerNum;
+    }
+
+    public void setPlayerNum(int playerNum) {
+        PlayerNum = playerNum;
+    }
+}
