ae52d12d32762c68d503525433ae9f0b3de8e091
LeonardoCesani <100211697+LeonardoCesani@users.noreply.github.com>
2022-03-20 17:43:06 +0100

    new classes skeleton

--- src/main/java/it/polimi/ingsw/model/Game.java
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -1,6 +1,55 @@
 package it.polimi.ingsw.model;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+
 public class Game {
+    private Status status;
+    private boolean isPro;
+    private int MovedPieces;
+    private GameStructure GameStructure;
+
+    public void endTurn(){}
+
+    public Player activePlayer(){
+        return null;
+    }
+
+    public Set<Player> playerList(){
+        return null;
+    }
+
+    public void changeActivePlayer(){ }
+
+    public Set<Student> drawStudents(){
+        return null;
+    }
+
+    public Map<ColorStudent, Player> colorDominance(){
+        return null;
+    }
+
+    public ArrayList<IslandCard> islandDominance(){
+        return null;
+    }
+
+    public void mergeIsland(){}
+
+    public void generateBoard(){}
+
+    public void addChosenCard(){}
+
+    public void pickCharacters(){}
+
+    public void startGame(){}
+
+    public Player AddPlayer(){
+        return null;
+    }
+
+
+
     public boolean IsIn(String ID){
         return false;
     }
--- src/main/java/it/polimi/ingsw/model/Status.java
+++ src/main/java/it/polimi/ingsw/model/Status.java
@@ -1,4 +1,4 @@
-package it.polimi.ingsw.controller;
+package it.polimi.ingsw.model;
 
 public enum Status {
     ONLINE,
