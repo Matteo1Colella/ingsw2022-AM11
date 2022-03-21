37d6d43f7b70670321eea048b38ef8fb079e4209
Matteo1Colella <100211692+Matteo1Colella@users.noreply.github.com>
2022-03-21 23:00:18 +0100

    Adding Login Management Features (#17)
    Co-authored-by: Matteo <Colella>

--- src/test/java/it/polimi/ingsw/AppTest.java
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -2,19 +2,36 @@ package it.polimi.ingsw;
 
 import static org.junit.Assert.assertTrue;
 
+import it.polimi.ingsw.model.GameManager;
+import it.polimi.ingsw.model.Lobby;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void TestSameID(){
+        ArrayList<Lobby> Lobbies = new ArrayList<Lobby>();
+        GameManager GM = new GameManager(Lobbies);
+        GM.Login("Cole", 3, true);
+        GM.Login("Leo", 3, true);
+        GM.Login("Cole", 3, true);
     }
+    @Test
+    public void TestFullLobby(){
+        ArrayList<Lobby> Lobbies = new ArrayList<Lobby>();
+        GameManager GM = new GameManager(Lobbies);
+        GM.Login("Cole", 3, true);
+        GM.Login("Leo", 3, true);
+        GM.Login("Ale", 3, true);
+        GM.Login("Gian", 3, true);
+    }
+
+
 }
