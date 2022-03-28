579b1a9f2e1558a19542067b757982531c86c771
LeonardoCesani <100211697+LeonardoCesani@users.noreply.github.com>
2022-03-28 17:43:33 +0200

    AssistantDeck and Card classes tested

---
+++ src/test/java/it/polimi/ingsw/AssistantDeckTest.java
@@ -0,0 +1,50 @@
+package it.polimi.ingsw;
+
+import it.polimi.ingsw.model.AssistantDeck;
+import it.polimi.ingsw.model.Card;
+import it.polimi.ingsw.model.DeckManager;
+import it.polimi.ingsw.model.Mage;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AssistantDeckTest {
+    @Test
+    public void chooseCards(){
+
+        DeckManager deckManager = new DeckManager();
+
+        AssistantDeck assistantDeck = deckManager.generateDeck(Mage.MAGE1);
+
+        Card choosenCard = assistantDeck.chooseCard(1);
+
+        assertNotNull(choosenCard);
+    }
+
+    @Test
+    public void choose10Cards(){
+
+        DeckManager deckManager = new DeckManager();
+
+        AssistantDeck assistantDeck = deckManager.generateDeck(Mage.MAGE1);
+
+        for(int i = 0; i < 10; i++){
+            Card choosenCard = assistantDeck.chooseCard(i);
+        }
+
+        assertEquals(0, assistantDeck.leftCard());
+    }
+
+    @Test
+    public void chooseSameCard(){
+        DeckManager deckManager = new DeckManager();
+
+        AssistantDeck assistantDeck = deckManager.generateDeck(Mage.MAGE1);
+
+        Card choosenCard = assistantDeck.chooseCard(1);
+
+        choosenCard = assistantDeck.chooseCard(1);
+
+        assertNull(choosenCard);
+    }
+}
