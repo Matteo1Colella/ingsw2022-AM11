82fe3f3e01c3def3b4ed3257877f880dcf37071d
Aleksandro Aliaj <100211777+AleksandroAliaj@users.noreply.github.com>
2022-04-03 14:06:36 +0200

    added winCondition and others Gamemethods (#47)

---
+++ src/test/java/it/polimi/ingsw/WinConditionTest.java
@@ -0,0 +1,31 @@
+package it.polimi.ingsw;
+
+import it.polimi.ingsw.controller.ComplexLobby;
+import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.Player;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+
+public class WinConditionTest {
+    @Test
+    public void winWithNoTowersTest() {
+        Game game = new Game(false,1,2);
+        ArrayList<Player> players = new ArrayList<>();
+        Player player1 = new Player(1,"ale");
+        Player player2 = new Player(1,"leo");
+        players.add(player1);
+        players.add(player2);
+        game.setComplexLobby(new ComplexLobby(2,false,1,players));
+        game.setGameStructure(game.generateBoard(false, 2));
+        assertEquals(2,game.getGameComponents().getSchoolBoards().size());
+
+        game.getGameComponents().getSchoolBoards().get(0).getTowers().clear();
+        assertEquals(0,game.getGameComponents().getSchoolBoards().get(0).getTowers().size());
+
+        assertEquals(player1, game.winCondition());
+    }
+}
