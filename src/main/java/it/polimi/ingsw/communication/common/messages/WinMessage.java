cf786ee110c5e4e19737b7657392aa712a8ef5da
Matteo <Colella>
2022-05-16 14:50:26 +0200

    Server update

---
+++ src/main/java/it/polimi/ingsw/communication/common/messages/WinMessage.java
@@ -0,0 +1,28 @@
+package it.polimi.ingsw.communication.common.messages;
+
+import it.polimi.ingsw.communication.common.MessageInterface;
+import it.polimi.ingsw.communication.common.MessageType;
+import it.polimi.ingsw.model.Player;
+
+public class WinMessage implements MessageInterface {
+
+    private String message;
+    private MessageType code;
+    private String winner;
+
+    public WinMessage(String player){
+       this.code = MessageType.WIN;
+       this.winner = player;
+       this.message = "Player: " + winner + " won!";
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public MessageType getCode() {
+        return code;
+    }
+}
