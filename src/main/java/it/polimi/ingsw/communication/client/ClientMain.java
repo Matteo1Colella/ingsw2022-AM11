09c58a7404f8183374d713e2f1cedd0b49d56f07
Aleksandro Aliaj <100211777+AleksandroAliaj@users.noreply.github.com>
2022-06-04 11:58:13 +0200

    char 4 fixed

--- src/main/java/it/polimi/ingsw/communication/client/ClientMain.java
+++ src/main/java/it/polimi/ingsw/communication/client/ClientMain.java
@@ -1148,8 +1148,9 @@ public class ClientMain extends Thread {
                 break;
             case 7,8:
                 System.out.println("(You can Select from 1 to 4 steps!)");
-                if(gameType && characterHandler.isChar4Used())
-                    System.out.println("You have also +2 extra steps... (max 6 steps)");
+                if(characterHandler!=null)
+                    if(gameType && characterHandler.isChar4Used())
+                        System.out.println("You have also +2 extra steps... (max 6 steps)");
                 scanner = new Scanner(System.in);
                 try {
                     numberSelectedSteps = scanner.nextInt();
@@ -1185,8 +1186,8 @@ public class ClientMain extends Thread {
                 break;
             case 9,10:
                 System.out.println("(You can Select from 1 to 5 steps!)");
-                if(characterHandler.isChar4Used())
-                    System.out.println("You have also +2 extra steps... (max 7 steps)");
+                if(gameType && characterHandler.isChar4Used())
+                        System.out.println("You have also +2 extra steps... (max 7 steps)");
                 scanner = new Scanner(System.in);
                 try {
                     numberSelectedSteps = scanner.nextInt();
