c6cc1b73047f5842e1e1383d4a2af68610975eb8
Matteo <Colella>
2022-06-30 15:23:06 +0200

    GUi

--- src/main/java/it/polimi/ingsw/view/ClientApp.java
+++ src/main/java/it/polimi/ingsw/view/ClientApp.java
@@ -1,10 +1,13 @@
 package it.polimi.ingsw.view;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.IOException;
 
@@ -27,6 +30,13 @@ public class ClientApp extends Application {
         primaryStage.setTitle("Create Connection");
         primaryStage.setScene(scene);
         primaryStage.getIcons().add(new Image("Assets/MN.png"));
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                Platform.exit();
+                System.exit(1);
+            }
+        });
         primaryStage.show();
     }
 }
